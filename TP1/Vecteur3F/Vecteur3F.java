628d18d7296c0bb99591eae79e51261b669fa715
Malo Dantec <mdantec56@gmail.com>
2024-01-24 19:06:03 +0100

    t

--- TP1/Vecteur3F/Vecteur3F.java
+++ TP1/Vecteur3F/Vecteur3F.java
@@ -11,6 +11,29 @@ public class Vecteur3F {
         return sqrt(this.premier * this.deuxieme * this.troisieme);
     }
     public modifier(float val, int pos) {
-
+        switch (pos) {
+            case pos == 1 : 
+                this.premier = val;
+                break;
+            case pos == 2 : 
+                this.deuxieme = val;
+                break;
+            case pos == 3 :
+                this.troisieme = val;
+                break;
+        }
+        // if pos == 1 {
+        //     this.premier = val;
+        // }
+        // if pos == 2 {
+        //     this.deuxieme = val;
+        // }
+        // if pos == 3 {
+        //     this.troisieme = val;
+        // }
+    }
+    @Override
+    public String toString() {
+        return "Vecteur3f : < " + this.premier + " " + this.deuxieme + " " + this.troisieme + "> De norme : " + NormeV3F();
     }
 }
